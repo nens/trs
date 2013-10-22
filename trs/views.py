c0814049d7750c787b736b286aef33a8f61c658d
Reinout van Rees <reinout@vanrees.org>
2013-10-22 17:35:22 +0200

    UI updates

--- trs/views.py
+++ trs/views.py
@@ -1,5 +1,29 @@
 from django.views.generic.base import TemplateView
 
 
-class HomeView(TemplateView):
+class BaseView(TemplateView):
     template_name = 'trs/base.html'
+    title = "TRS tijdregistratiesysteem"
+
+
+class HomeView(BaseView):
+    template_name = 'trs/home.html'
+
+
+class ProjectsView(BaseView):
+    """Show project list and individual projects.
+
+    TODO: thinkwork.
+
+    A single page for showing a list and for showing individual projects? Een
+    soort deel-single-page-app?
+
+    Then what are the specific things that this view ought to do? Configure
+    the overview column, for instance?
+
+    Template snippets klaarzetten?
+
+    => djangorestframework, angularjs :-)
+
+    """
+    template_name = 'trs/home.html'
