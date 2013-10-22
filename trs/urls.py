1ac50f63d86404322ca40a6c8e17c74e47856108
Reinout van Rees <reinout@vanrees.org>
2013-10-22 12:36:35 +0200

    Starting empty homepage

--- trs/urls.py
+++ trs/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
 
+from trs import views
+
 
 urlpatterns = patterns(
     '',
+    url(r'^$',
+        views.HomeView.as_view(),
+        name='trs.home'),
     )
