a5a6aee615572a388af7295fd89c5a588174b9ee
Reinout van Rees <reinout@vanrees.org>
2013-10-25 15:25:10 +0200

    Added couple of fields to person/project

--- trs/models.py
+++ trs/models.py
@@ -1,12 +1,33 @@
 from django.db import models
 
-
 # TODO: add setting ADMIN_USER_CAN_DELETE_PERSONS_AND_PROJECTS
 # TODO: add django-appconf
 
+
 class Person(models.Model):
-    pass
+    slug = models.SlugField(
+        verbose_name="ID voor in de URL")
+    name = models.CharField(
+        verbose_name="naam",
+        max_length=255)
+    description = models.CharField(
+        verbose_name="omschrijving",
+        blank=True,
+        max_length=255)
+    # Description on persons is useful for noting that someone doesn't work
+    # for us anymore, for instance. And other corner cases.
 
 
 class Project(models.Model):
-    pass
+    slug = models.SlugField(
+        verbose_name="ID voor in de URL")
+    code = models.CharField(
+        verbose_name="projectcode",
+        max_length=255)
+    description = models.CharField(
+        verbose_name="omschrijving",
+        blank=True,
+        max_length=255)
+    added = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="toegevoegd op")
