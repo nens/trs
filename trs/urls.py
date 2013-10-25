64f40962d436e009c6418904516bcfcd59b30918
Reinout van Rees <reinout@vanrees.org>
2013-10-25 10:26:10 +0200

    Added admin urls

--- trs/urls.py
+++ trs/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
+from django.contrib import admin
 
 from trs import views
 
 
 urlpatterns = patterns(
     '',
+    (r'^admin/', include(admin.site.urls)),
     url(r'^$',
         views.HomeView.as_view(),
         name='trs.home'),
