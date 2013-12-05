c771d7281c4faf8d7097739dedc7419fc4a5b51b
Reinout van Rees <reinout@vanrees.org>
2013-12-05 14:50:29 +0100

    Added production settings via trs-site mr.developer checkout

---
+++ trs/productionsettings.py
@@ -0,0 +1,6 @@
+# He he, no secret production settings here in this open source repository of
+# course :-)
+# We import from a "trs-site" project that's in a private github repo.
+# From here, we import the secret key and so.
+
+from trs_site.productionsettings import *
