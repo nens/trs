7b75ac2c93424f851563d1f9f9a88772f117bdff
Reinout van Rees <reinout@vanrees.org>
2013-12-06 08:59:03 +0100

    Added ALLOWED_HOSTS setting that I forgot

--- trs/testsettings.py
+++ trs/testsettings.py
@@ -8,6 +8,7 @@ STATIC_URL = '/static/'
 ROOT_URLCONF = 'trs.urls'
 SECRET_KEY = 'sleutel van het secreet'
 DEBUG = True
+ALLOWED_HOSTS = ['trs.lizard.net']
 TEMPLATE_DEBUG = True
 DATABASES = {
     'default': {'ENGINE': 'django.db.backends.sqlite3',
