c8f0acede46219335288cb53410779642648a57d
Reinout van Rees <reinout@vanrees.org>
2013-12-16 16:58:51 +0100

    Removed ppc from fill_cache

--- trs/management/commands/fill_cache.py
+++ trs/management/commands/fill_cache.py
@@ -18,9 +18,3 @@ class Command(BaseCommand):
         for index, person in enumerate(models.Person.objects.all()):
             core.get_pyc(person)
             logger.info("%s out of %s done", index + 1, num_persons)
-        num_projects = models.Project.objects.all().count()
-        logger.info("%s projects in total.", num_projects)
-        for index, project in enumerate(models.Project.objects.all()):
-            for person in project.assigned_persons():
-                core.get_ppc(project, person)
-            logger.info("%s out of %s done", index + 1, num_projects)
