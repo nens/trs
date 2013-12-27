9c5f299b7062fd3fb22732b56feafc4e7a05c7f8
Reinout van Rees <reinout@vanrees.org>
2013-12-28 00:14:16 +0100

    Made fill_cache command more effective

--- trs/management/commands/fill_cache.py
+++ trs/management/commands/fill_cache.py
@@ -16,5 +16,13 @@ class Command(BaseCommand):
         num_persons = models.Person.objects.all().count()
         logger.info("%s persons in total.", num_persons)
         for index, person in enumerate(models.Person.objects.all()):
+            person.to_book()
+            person.to_work_up_till_now()
             core.get_pyc(person)
             logger.info("%s out of %s done", index + 1, num_persons)
+
+        num_projects = models.Project.objects.all().count()
+        logger.info("%s projects in total.", num_projects)
+        for index, project in enumerate(models.Project.objects.all()):
+            project.work_calculation()
+            logger.info("%s out of %s done", index + 1, num_projects)
