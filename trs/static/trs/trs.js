ec74a663cb45fb2e2781418f61fdc6ac2c8ea12c
Reinout van Rees <reinout@vanrees.org>
2013-12-31 13:32:49 +0100

    Got non-editable 'fields' to count, too

--- trs/static/trs/trs.js
+++ trs/static/trs/trs.js
@@ -1,7 +1,13 @@
 function SumTotalHours() {
     var total = 0;
-    $('.hour-for-total input').each(function() {
-        value = $(this).val();
+    $('.hour-for-total').each(function() {
+        if ($(this).children().length > 0 ) {
+            // I have children (= an input element).
+            value = $('input', this).val();
+        } else {
+            // I am not editable, I only have my hours as text.
+            value = $(this).text();
+        }
         if (!isNaN(value) && (value.length != 0))  {
             total += parseInt(value);
         }
