821d0b373cc7101f441dc0a2921babd2149f81a2
Reinout van Rees <reinout@vanrees.org>
2013-12-31 13:59:16 +0100

    Added warning if you're about to leave the page without saving your changes

--- trs/static/trs/trs.js
+++ trs/static/trs/trs.js
@@ -1,5 +1,6 @@
 function SumTotalHours() {
     var total = 0;
+    var value;
     $('.hour-for-total').each(function() {
         if ($(this).children().length > 0 ) {
             // I have children (= an input element).
@@ -16,7 +17,17 @@ function SumTotalHours() {
 }
 
 $(document).ready(function () {
+    window.form_changed = false;
     $(".hour-for-total input").keyup(function() {
-        SumTotalHours()
+        SumTotalHours();
+        window.form_changed = true;
+    });
+    window.onbeforeunload = function(e) {
+        if (window.form_changed) {
+            return "Er is wat veranderd. Klik misschien eerst op submit of enter?";
+        }
+    };
+    $("#booking-form").submit(function(e) {
+        window.form_changed = false;
     });
 });
