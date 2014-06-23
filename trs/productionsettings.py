15676f832a95e0c6d4e360828217976edacb00a0
Reinout van Rees <reinout@vanrees.org>
2014-06-23 15:26:01 +0200

    productionsettings is automatically loaded by nose. Prevent it from modifying the installed apps list in-place

--- trs/productionsettings.py
+++ trs/productionsettings.py
@@ -3,6 +3,7 @@
 # We import from a "trs-site" project that's in a private github repo.
 # From here, we import the secret key and so.
 from trs.testsettings import *
+from copy import copy
 
 DEBUG = False
 CACHES = {
@@ -20,6 +21,8 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStora
 
 for unwanted_in_production in ['debug_toolbar.middleware.DebugToolbarMiddleware',
                                'debug_toolbar']:
+    MIDDLEWARE_CLASSES = copy(MIDDLEWARE_CLASSES)
+    INSTALLED_APPS = copy(INSTALLED_APPS)
     for where in [MIDDLEWARE_CLASSES, INSTALLED_APPS]:
         if unwanted_in_production in where:
             where.remove(unwanted_in_production)
