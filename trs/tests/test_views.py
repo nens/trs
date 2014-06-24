c08b608e115a0fcafa5613cc93b7a7dccebce7e3
Reinout van Rees <reinout@vanrees.org>
2014-06-24 10:41:49 +0200

    Got the tests working again

--- trs/tests/test_views.py
+++ trs/tests/test_views.py
@@ -1,12 +1,14 @@
 import datetime
 
-from django.test import TestCase
 from django.test.client import RequestFactory
+from django.test import TestCase
 from django.contrib.auth.models import AnonymousUser
 import mock
 
 from trs import views
+from trs.management.commands.update_weeks import ensure_year_weeks_are_present
 from trs.tests import factories
+
 
 REINOUTS_BIRTHDATE = datetime.date(year=1972, month=12, day=25)
 
@@ -33,6 +35,7 @@ class BaseMixinTestCase(TestCase):
         self.assertEqual(self.view.active_person, person)
 
     def test_active_projects(self):
+        ensure_year_weeks_are_present()
         person = factories.PersonFactory.create()
         project1 = factories.ProjectFactory.create(code='p1')
         project2 = factories.ProjectFactory.create(code='p2')
@@ -42,7 +45,7 @@ class BaseMixinTestCase(TestCase):
                                         assigned_on=project2)
         self.view.request.user = person.user
         self.assertEqual(list(self.view.active_projects),
-                         [project1, project2])
+                         [project2, project1])
 
 
 class PersonsViewTestCase(TestCase):
@@ -52,7 +55,9 @@ class PersonsViewTestCase(TestCase):
         self.person2 = factories.PersonFactory.create()
 
     def test_smoke(self):
-        view = views.PersonsView()
+        ensure_year_weeks_are_present()
+        request = RequestFactory().get('/')
+        view = views.PersonsView(request=request)
         self.assertEqual(view.lines[1]['person'], self.person2)
 
 
@@ -73,8 +78,11 @@ class ProjectsViewTestCase(TestCase):
         self.project2 = factories.ProjectFactory.create()
 
     def test_smoke(self):
-        view = views.ProjectsView()
-        self.assertEqual(view.lines[1]['project'], self.project2)
+        person = factories.PersonFactory.create()
+        request = RequestFactory().get('/')
+        request.user = person.user
+        view = views.ProjectsView(request=request)
+        self.assertEqual(view.lines[1]['project'], self.project1)
 
 
 class ProjectViewTestCase(TestCase):
