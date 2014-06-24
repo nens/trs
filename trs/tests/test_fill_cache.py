9c034c5ceb15864b2af33d603b3e9eade1567e58
Reinout van Rees <reinout@vanrees.org>
2014-06-24 14:43:45 +0200

    Tested mgmt command

---
+++ trs/tests/test_fill_cache.py
@@ -0,0 +1,21 @@
+import unittest  # Note: this is 3.3's unittest2!
+
+from django.core.cache import cache
+
+from trs.management.commands import fill_cache
+from trs.management.commands.update_weeks import ensure_year_weeks_are_present
+from trs.tests import factories
+
+
+class TestFillCache(unittest.TestCase):
+
+    def setUp(self):
+        self.command = fill_cache.Command()
+
+    def test_handle(self):
+        ensure_year_weeks_are_present()
+        self.person = factories.PersonFactory.create()
+        self.project = factories.ProjectFactory.create()
+        self.command.handle()
+        key = self.project.cache_key('work_calculation')
+        self.assertTrue(cache.get(key))
