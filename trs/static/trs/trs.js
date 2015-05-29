8ae9496c03e199a1205203f291bce0e93f1d1f79
Reinout van Rees <reinout@vanrees.org>
2015-05-29 11:10:44 +0200

    Moved selection pager to separate function. Storing start url.

--- trs/static/trs/trs.js
+++ trs/static/trs/trs.js
@@ -16,24 +16,15 @@ function SumTotalHours() {
     $('#hour-total').text(total);
 }
 
-$(document).ready(function () {
-    window.form_changed = false;
-    $(".hour-for-total input").keyup(function() {
-        SumTotalHours();
-        window.form_changed = true;
-    });
-    window.onbeforeunload = function(e) {
-        if (window.form_changed) {
-            return "Er is wat veranderd. Klik misschien eerst op submit of enter?";
-        }
-    };
-    $("#booking-form").submit(function(e) {
-        window.form_changed = false;
-    });
-    $('.table-fixed-header').fixedHeader();
 
+function configureSelectionPager() {
     var selection_pager = JSON.parse(localStorage.getItem('selection_pager'));
+    var selection_pager_start_url = localStorage.getItem(
+        'selection_pager_start_url');
+
     if (for_selection_pager) {
+        // for_selection_pager is defined in base.html for pagers where there
+        // is such a list available for the selection pager.
         $("#selection-pager").show();
         $("#enable-selection-pager").show();
     }
@@ -43,12 +34,15 @@ $(document).ready(function () {
     }
 
     $("#enable-selection-pager a").click(function(e) {
-        localStorage.setItem('selection_pager', JSON.stringify(for_selection_pager));
+        localStorage.setItem('selection_pager',
+                             JSON.stringify(for_selection_pager));
+        localStorage.setItem('selection_pager_start_url',
+                             document.location.href);
         document.location.reload();
     });
     $("#disable-selection-pager a").click(function(e) {
         localStorage.removeItem('selection_pager');
-        document.location.reload();
+        document.location.href = selection_pager_start_url;
     });
     if (selection_pager) {
         var selection_pager_contents = '';
@@ -70,5 +64,23 @@ $(document).ready(function () {
         console.log(selection_pager_contents);
         $('#selection-pager-contents').html(selection_pager_contents);
     }
+}
 
+
+$(document).ready(function () {
+    window.form_changed = false;
+    $(".hour-for-total input").keyup(function() {
+        SumTotalHours();
+        window.form_changed = true;
+    });
+    window.onbeforeunload = function(e) {
+        if (window.form_changed) {
+            return "Er is wat veranderd. Klik misschien eerst op submit of enter?";
+        }
+    };
+    $("#booking-form").submit(function(e) {
+        window.form_changed = false;
+    });
+    $('.table-fixed-header').fixedHeader();
+    configureSelectionPager();
 });
