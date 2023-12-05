8d80c17fef97703ef2de2cda55025e97f3061813
Reinout van Rees <reinout@vanrees.org>
2023-12-05 15:40:04 +0100

    ruff format

--- manage.py
+++ manage.py
@@ -4,9 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "trs.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trs.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
